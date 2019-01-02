5e66d352e4ba3a2955c4d78e7d17a9f0021c678f
ArfaCheung <arfa.cheung.069@hotmail.com>
2019-01-02 17:08:37 +0800

    修复好热点功能
    修复好热点功能

--- pages/map/map.js
+++ pages/map/map.js
@@ -8,7 +8,7 @@ Page({
     latitude: '',
     longitude: '',
     placeData: {},
-    searchQuery: ''
+    searchQuery: '酒店'
   },
   makertap: function (e) {
     var that = this;
@@ -16,12 +16,15 @@ Page({
     that.showSearchInfo(wxMarkerData, id);
     that.changeMarkerColor(wxMarkerData, id);
   },
-  onLoad: function () {
+
+  newMap:function(){
     var that = this;
     // 新建百度地图对象 
     var BMap = new bmap.BMapWX({
       ak: 'ioRBCXtvq2ima9fTdfGAfaGOidG0lTeG'
     });
+    var query = that.data.searchQuery;
+
     var fail = function (data) {
       console.log(data)
     };
@@ -37,17 +40,30 @@ Page({
         longitude: wxMarkerData[0].longitude
       });
     }
+    console.log(query)
     // 发起POI检索请求 
     BMap.search({
-      "query": this.data.searchQuery.value,
+      "query": query,
       fail: fail,
       success: success,
-      // 此处需要在相应路径放置图片文件 
-      iconPath: '../../img/marker_red.png',
-      // 此处需要在相应路径放置图片文件 
-      iconTapPath: '../../img/marker_red.png'
+      iconPath: '../../img/1.png',
+      iconTapPath: '../../img/1.png'
     });
   },
+
+  onLoad: function () {
+    var that = this;
+    that.newMap();
+  },
+
+  formSubmit:function(query){
+    var that = this;
+    that.setData({
+      searchQuery: query.detail.value.query
+    })
+    that.newMap();
+  },
+
   showSearchInfo: function (data, i) {
     var that = this;
     that.setData({
@@ -63,13 +79,11 @@ Page({
     var markers = [];
     for (var j = 0; j < data.length; j++) {
       if (j == i) {
-        // 此处需要在相应路径放置图片文件 
-        data[j].iconPath = "../../img/marker_yellow.png";
+        data[j].iconPath = "../../img/2.png";
       } else {
-        // 此处需要在相应路径放置图片文件 
-        data[j].iconPath = "../../img/marker_red.png";
+        data[j].iconPath = "../../img/1.png";
       }
-      markers[j](data[j]);
+      markers[j] = data[j];
     }
     that.setData({
       markers: markers
